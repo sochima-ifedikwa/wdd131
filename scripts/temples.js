2b696032c07741283dab0578428bb085e3a75fb4
sochima-ifedikwa <ifedikwasochima@gmail.com>
2025-01-30 23:03:31 -0600

    `Updated menu functionality and styles for temples page`

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,26 +1,26 @@
-// const header = document.querySelector("#menu");
-// const links = document.querySelectorAll("a");
-// const nav = document.querySelector("nav");
-// const ul = document.querySelector('ul');
-// const menuButton = document.querySelector("#menu");
+const header = document.querySelector("#selectedMenu");
+const links = document.querySelectorAll("a");
+const nav = document.querySelector("nav");
+const ul = document.querySelector('ul');
+const menuButton = document.querySelector("#menu");
 
-// menuButton.addEventListener('click', () => {
-//     nav.classList.toggle('show');
-//     ul.classList.toggle('show');
-//     menuButton.classList.toggle('show');
-// });
+menuButton.addEventListener('click', () => {
+    nav.classList.toggle('show');
+    ul.classList.toggle('show');
+    menuButton.classList.toggle('show');
+});
 
-// [...links].map((link) => {
-//     // set header default value
-//     header.textContent = "Home";
-//     // add an event listener to update header value
-//     link.addEventListener('click', () => {
-//         header.textContent = link.title
-//         // close menu on small screens once link selected
-//         if (window.innerWidth < 640) {
-//             nav.classList.toggle("show");
-//             ul.classList.toggle("show");
-//             menuButton.classList.toggle("show");     
-//         }
-//     })
-// });
+[...links].map((link) => {
+    // set header default value
+    header.textContent = "Home";
+    // add an event listener to update header value
+    link.addEventListener('click', () => {
+        header.textContent = link.title
+        // close menu on small screens once link selected
+        if (window.innerWidth < 599) {
+            nav.classList.toggle("show");
+            ul.classList.toggle("show");
+            menuButton.classList.toggle("show");     
+        }
+    })
+});
